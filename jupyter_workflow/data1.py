f393475938319193fe7b1495141a18e68334c0cf
Vasanth <vasanth1627@gmail.com>
2018-07-07 21:47:37 +0530

    Refactored the data download method for speed

--- jupyter_workflow/data1.py
+++ jupyter_workflow/data1.py
@@ -5,7 +5,13 @@ fremont_url = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv'
 def  get_fremont_data(filename = 'Fremont.csv', url  = fremont_url, force_download = False):
 	if force_download or not os.path.exists(filename):
 		urlretrieve(url, filename)
-	data = pd.read_csv('Fremont.csv', parse_dates= True, index_col= 'Date')
+	data = pd.read_csv('Fremont.csv',  index_col= 'Date')
+	try:
+		data.index = pd.to_datetime(data.index, format = '%m/%d/%Y %H:%M:%S %p')
+	except TypeError:
+    data.index = pd.to_datetime(data.index)
+    
+    
 	data.columns = ['West' , 'East']
 	data['Total'] = data["East"] + data["West"]
 	return data
